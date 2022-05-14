770a01219c4d64772333c942661772131a1d8101
ansatsu01 <89972197+ansatsu01@users.noreply.github.com>
2022-05-14 12:39:07 +0300

    animation for bird

--- bird.cpp
+++ bird.cpp
@@ -6,11 +6,9 @@ birdy.setX(for_bird.x());
 birdy.setY(for_bird.y());
 }
 
-void Bird::create_bird(QPainter& painter){
+void Bird::create_bird(QPainter& painter, QImage* image){
 
-    QImage bird_Im("D:/Viktoria/BSU/programming/yellowbird-midflap1.png", "PNG");   //bird :  56x41
-
-    painter.drawImage(birdy, bird_Im);
+   painter.drawImage(birdy, *image);
 }
 
 
